2be151d1e5aac8e860ea1b207dbcfd9caa0de68d
Steven Mazliach <smazliac@ucsc.edu>
2017-07-11 19:30:23 -0700

    got basic scraper working with selenium locally

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, Response
+import scraper
  
 app = Flask(__name__)      
  
 @app.route('/')
 def home():
   return render_template('home.html')
+
+@app.route('/scrape/<id>', methods=['GET'])
+def scrape(id=None):
+	if(id == None):
+		return "<html>No id provided</html>"
+	else:
+		chats = scraper.run_scraper(id)
+		return Response(json.dumps(chats), mimetype='application/json')
  
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
---
+++ app/scraper.py
@@ -0,0 +1,22 @@
+import json
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+def run_scraper(id):
+	display = Display(visible=0, size=(1024, 768))
+	display.start()
+
+	browser = webdriver.Chrome()
+
+	url = "https://www.youtube.com/live_chat?v=" + str(id)
+	browser.get(url)
+
+	#innerHTML = browser.execute_script("return document.body.innerHTML")
+	chats = []
+	for chat in browser.find_elements_by_css_selector('yt-live-chat-text-message-renderer'):
+		message = chat.find_element_by_css_selector("#message").get_attribute('innerHTML')
+		chats.append(("{ message: '" + message.encode('utf-8').strip() +"' },"))
+
+	browser.quit()
+	display.stop()
+	return chats
