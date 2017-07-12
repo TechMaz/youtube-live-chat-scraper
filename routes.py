c1974257e8e37ddda24d7c453241a8d562d4c11c
Steven Mazliach <smazliac@ucsc.edu>
2017-07-11 17:58:27 -0700

    update settings in routes.py for Heroku

--- routes.py
+++ routes.py
@@ -7,4 +7,6 @@ def home():
   return render_template('home.html')
  
 if __name__ == '__main__':
-  app.run(debug=True)
+ 	# Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
