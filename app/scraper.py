6b9ab8d5a037d9094bf36fbacf0857ced116db94
Steven Mazliach <smazliac@ucsc.edu>
2017-07-11 23:40:48 -0700

    seeing if we can get chrome to not crash on heroku try4

--- app/scraper.py
+++ app/scraper.py
@@ -14,22 +14,15 @@ def run_scraper(id):
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument('--headless')
 	chrome_options.add_argument('--disable-extensions')
-	#chrome_options.add_argument('window-size=1200x600')
+	chrome_options.add_argument('window-size=1200x600')
 	chrome_bin = os.getenv('GOOGLE_CHROME_SHIM', None)
-	chromedriver_path = '.chromedriver/bin/chromedriver'
+
 	if(chrome_bin):
+		chromedriver_path = '.chromedriver/bin/chromedriver'
 		chrome_options.binary_location = '.apt/usr/bin/google-chrome-stable'
-		'''
-		desired_capabilities = DesiredCapabilities.CHROME
-		desired_capabilities['chromeOptions'] = {
-    		"binary": '.apt/usr/bin/google-chrome-stable'
-		}
-		browser = webdriver.Chrome(desired_capabilities=desired_capabilities)
-		'''
-		browser = webdriver.Chrome(executable_path=chromedriver_path,desired_capabilities=chrome_options.to_capabilities())
+		browser = webdriver.Chrome(executable_path=chromedriver_path,chrome_options=chrome_options)
 	else:
-		#chrome_options.add_argument('window-size=1200x600')
-		browser = webdriver.Chrome(desired_capabilities=chrome_options.to_capabilities())
+		browser = webdriver.Chrome(chrome_options=chrome_options)
 
 	url = "https://www.youtube.com/live_chat?v=" + str(id)
 	browser.get(url)
@@ -37,9 +30,24 @@ def run_scraper(id):
 	#innerHTML = browser.execute_script("return document.body.innerHTML")
 	chats = []
 	for chat in browser.find_elements_by_css_selector('yt-live-chat-text-message-renderer'):
+		author_name = chat.find_element_by_css_selector("#author-name").get_attribute('innerHTML')
 		message = chat.find_element_by_css_selector("#message").get_attribute('innerHTML')
-		chats.append(("{ message: '" + message.encode('utf-8').strip() +"' },"))
+		author_name_encoded = author_name.encode('utf-8').strip()
+		message_encoded = message.encode('utf-8').strip()
+		obj = json.dumps({'author_name': author_name_encoded, 'message': message_encoded })
+		chats.append(json.loads(obj))
 
 	browser.quit()
 	#display.stop()
 	return chats
+
+
+
+
+'''
+desired_capabilities = DesiredCapabilities.CHROME
+desired_capabilities['chromeOptions'] = {
+   	"binary": '.apt/usr/bin/google-chrome-stable'
+}
+browser = webdriver.Chrome(desired_capabilities=desired_capabilities)
+'''
